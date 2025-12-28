8b4f8247953aebb2af27f68ad77415d1770c0813
DivyaAhire11 <divyaahire121@gmail.com>
2025-12-28 12:01:04 +0530

    First Program

--- Multithreading/MyThread1.java
+++ Multithreading/MyThread1.java
@@ -19,8 +19,23 @@ public class MyThread1 {
         A ob1 = new A();
         B ob2 = new B();
 
+        System.out.println(ob1.getName());  //Thread-0
+
+        System.out.println(ob2.getName());  //Thread-1
+
+        /**
+         * Priority goes from 1 to 10
+         * 1 is the less priority
+         * 10 is the High priority
+         * 5 is the normal priority(ByDefault priority)
+         */
+        System.out.println(ob1.getPriority()); //5
+        System.out.println(ob2.getPriority());  //5
+
         ob1.start();
         ob2.start();
+
+
 
     }
 }
