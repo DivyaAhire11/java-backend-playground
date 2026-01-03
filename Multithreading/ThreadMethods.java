4662a970421a8f606b94d4651e61661e7c28f524
DivyaAhire11 <divyaahire121@gmail.com>
2026-01-03 17:49:15 +0530

    About Priority

--- Multithreading/ThreadMethods.java
+++ Multithreading/ThreadMethods.java
@@ -1,14 +1,26 @@
 package Multithreading;
 
-public class ThreadMethods extends Thread{
-    public void run(){
-          for(int i=0;i<5;i++){
-               System.out.println(Thread.currentThread().getName()+" - Priority : "+Thread.currentThread().getPriority()+" - Count : "+i);
-          }
+public class ThreadMethods extends Thread {
+    public ThreadMethods(String nm) {
+        super(nm);
+    }
+
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - Priority : "
+                    + Thread.currentThread().getPriority() + " - Count : " + i);
+        }
     }
 
     public static void main(String[] args) {
-            ThreadMethods t1 = new ThreadMethods();
-            t1.start();
+        ThreadMethods t1 = new ThreadMethods("High Priority Thread"); // naming of thread Thread-0 -->High Priority Thread
+        ThreadMethods t2 = new ThreadMethods("Low Priority Thread"); // naming of thread Thread-1 --> Low Priority Thread
+        ThreadMethods t3 = new ThreadMethods("Medium Priority Thread"); // naming of thread Thread-2 --> Medium Priority Thread
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t2.setPriority(Thread.MIN_PRIORITY);
+        t3.setPriority(Thread.NORM_PRIORITY);
+        t1.start();
+        t2.start();
+        t3.start();
     }
 }
