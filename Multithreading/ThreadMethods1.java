d3a8dd1f93d46477e3e178e1ba13dff9a97bb150
DivyaAhire11 <divyaahire121@gmail.com>
2026-01-03 19:51:21 +0530

    Thread methods in java

---
+++ Multithreading/ThreadMethods1.java
@@ -0,0 +1,27 @@
+package Multithreading;
+
+/**
+ * Daemon threads : background threads(JVM not wait)
+ * JVM does NOT wait for daemon threads to finish
+ * When all non-daemon (user) threads end, the JVM terminates, even if daemon threads are still running.
+ * ex : garbage collector , backgroud cleanup tasks
+ */
+public class ThreadMethods1 extends Thread {
+    int cnt = 0;
+
+    public void run() {
+        while (true) {
+            cnt++;
+            System.out.println("Hello!! : " + cnt);
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadMethods1 t = new ThreadMethods1();
+        t.setDaemon(true);  //Marks the thread as a daemon thread
+        //Must be called before start(), otherwise an exception occurs
+        t.start(); //call run()
+        System.out.println("Main() end!!");
+    }
+}
+//output : see few or no "Hello!!" lines
