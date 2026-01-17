1793e8f469c27ad56c365d77be8bb4914c3a2573
DivyaAhire11 <divyaahire121@gmail.com>
2026-01-17 21:35:42 +0530

    Modification

--- ThreadingPrograms/DaemonDemo.java
+++ ThreadingPrograms/DaemonDemo.java
@@ -1,22 +1,27 @@
 package ThreadingPrograms;
 
-class DaemonDemo extends Thread{
-    public void run(){
-        if(Thread.currentThread().isDaemon()){
-           System.out.println(Thread.currentThread().getName() +" is a Daemon Thread.");
-        }else{
-           System.out.println(Thread.currentThread().getName() +" is a User Thread.");
+// If t1 , t2 both are daemon thread then : O/P --> nothing display
+// if only daemon thread and remove user thread : O/P --> nothing display
+// JVM may exit immediately
+
+class DaemonDemo extends Thread {
+    public void run() {
+        if (Thread.currentThread().isDaemon()) {
+            System.out.println(Thread.currentThread().getName() + " is a Daemon Thread.");
+        } else {
+            System.out.println(Thread.currentThread().getName() + " is a User Thread.");
         }
     }
 }
-class Demo{
+
+class Demo {
     public static void main(String[] args) {
         DaemonDemo t1 = new DaemonDemo();
         DaemonDemo t2 = new DaemonDemo();
 
-          t1.setDaemon(true);
+        t1.setDaemon(true);
 
-          t1.start();  //Daemon Thread
-          t2.start();  //User Thread
+        t1.start(); // Daemon Thread
+        t2.start(); // User Thread
     }
 }
