a469d2940ddae7e4970b6acd78cc88cb3ed2de36
DivyaAhire11 <divyaahire121@gmail.com>
2026-01-26 09:09:19 +0530

    Question

---
+++ JavaJourney/7.1_JavaPractice.java
@@ -0,0 +1,12 @@
+package JavaJourney;
+/*
+  write a java program that implements a multi-thread application that has three threads.
+  first thread generates random integer number after every one second,if the number is even
+  second thread computes the square of that number and print it. If the number is Odd
+  the third thread computes of cube of that number and print it. 
+*/
+class NumberGenerator{
+    public static void main(String[] args) {
+        
+    }
+}
