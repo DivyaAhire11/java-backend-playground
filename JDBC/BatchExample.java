1d0e14d38d916c88d1059379f5c836aeda9dd504
DivyaAhire11 <divyaahire121@gmail.com>
2026-02-01 17:29:24 +0530

    Batch Insert using Statement

---
+++ JDBC/BatchExample.java
@@ -0,0 +1,32 @@
+package JDBC;
+
+//Insert update many records at once
+//improve performance
+/*  BATCH PROCESSING :
+        Multiple SQL -> one DB call
+        Fast
+
+*/
+import java.sql.*;
+
+public class BatchExample {
+    public static void main(String[] args) {
+        try {
+             Connection con = DriverManager.getConnection(System.getenv("DB_URL"), System.getenv("DB_USER"),
+                    System.getenv("DB_PASSWORD"));
+
+            Statement stm = con.createStatement();
+
+            stm.addBatch("INSERT INTO student VALUES (6,'Pratibha',88)");
+            stm.addBatch("INSERT INTO student VALUES(7,'Surbhi',74)");
+            stm.addBatch("INSERT INTO student VALUES(8,'Geeta',44)");
+
+            int[] result = stm.executeBatch();
+            System.out.println("Records Inserted : "+result.length);
+
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
